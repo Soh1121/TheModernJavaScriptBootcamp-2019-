e9a1398ee6d96a6b5dab8bd670daeb55e471e87f
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-05-24 20:44:37 +0900

    [update]テンプレート文字列の学習

--- functions/arguments.js
+++ functions/arguments.js
@@ -8,7 +8,8 @@ console.log(result)
 
 // Default arguments
 let getScoreText = function (name = 'Anoymous', score = 0) {
-    return 'Name: ' + name + ' - Score: ' + score
+    return `Name: ${name} - Score: ${score}`
+    // return 'Name: ' + name + ' - Score: ' + score
 }
 
 let scoreText = getScoreText(99)
@@ -16,10 +17,13 @@ console.log(scoreText)
 
 // Challenge area
 // total, tipPercent .2
-
+// A 25% tip on $40 would be $10
 let getTip = function (total, tipPercent = .2) {
-    return total * tipPercent
+    let percent = tipPercent * 100
+    let tip = total * tipPercent
+    return `A ${percent}% tip on $${total} would be $${tip}`
+    // return `A ${tipPercent * 100}% tip on $${total} would be $${total * tipPercent}`
 }
 
-let tip = getTip(40, .25)
+let tip = getTip(60)
 console.log(tip)
