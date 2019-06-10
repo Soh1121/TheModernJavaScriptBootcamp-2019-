c701e18fc070cc71d8cb1a8f90603d6f8250d868
Soh1121 <satou.shg@gmail.com>
2019-06-10 21:50:32 +0900

    [update]配列の学習
    チャレンジ問題を自分なりに

---
+++ arrays/todo.js
@@ -0,0 +1,11 @@
+// Create an array with five todos
+// You have x todos
+// Print the first and second to last items -> Todo: walk the dog
+
+const todo = [
+    'todo 1', 'todo 2', 'todo 3', 'todo 4', 'walk the dog'
+]
+
+console.log(todo[0])
+console.log(todo[1])
+console.log(todo[todo.length - 1])
