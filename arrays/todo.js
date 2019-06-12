b3f19492b1c5a1327f86bb858fabd458575ebd75
Soh1121 <satou.shg@gmail.com>
2019-06-12 22:33:25 +0900

    [update]配列のメソッドの学習
    模範解答

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,11 +1,17 @@
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
 
+todos.splice(2, 1)
+todos.push('Buy coffee')
+todos.shift()
+
+/*
 // Delete the 3rd item
 todos.splice(2, 1)
 // Add a new item onto the end
 todos.push('New item')
 // Remove the first item from the list
 todos.shift()
+ */
 
 console.log(`You have ${todos.length} todos!`)
 console.log(todos)
