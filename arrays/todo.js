6b9b60edb8f8fe8fb737558fc5274fd5e364d16c
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-14 15:38:56 +0900

    [update]forループの学習
    模範解答

--- arrays/todo.js
+++ arrays/todo.js
@@ -13,6 +13,13 @@ todos.forEach(function (todo, index) {
 // 1. The first item
 // 2. The second item
 // ...
-for (let count = 1; count <= todos.length; count++) {
+/* for (let count = 1; count <= todos.length; count++) {
     console.log(`${count}. ${todos[count - 1]}`)
 }
+ */
+
+ for (let count = 0; count < todos.length; count++) {
+    const num = count + 1
+    const todo = todos[count]
+    console.log(`${num}. ${todo}`)
+ }
