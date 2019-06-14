8e1df0fcb155b1c537ec08d67744a439562d7aae
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-14 15:35:27 +0900

    [update]forループの学習
    チャレンジ問題を自分なりに

--- arrays/todo.js
+++ arrays/todo.js
@@ -12,7 +12,7 @@ todos.forEach(function (todo, index) {
 
 // 1. The first item
 // 2. The second item
-/* todos.forEach(function (item, index) {
-    console.log(`${index + 1}. ${item}`)
-})
- */
+// ...
+for (let count = 1; count <= todos.length; count++) {
+    console.log(`${count}. ${todos[count - 1]}`)
+}
