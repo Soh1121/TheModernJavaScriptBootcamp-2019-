4ec9e5293f4c4cd8b8613fbf178c9a57e4cf7559
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-17 14:08:22 +0900

    [update]配列の検索１
    連想配列はメモリの参照先を指定しているだけ。検索はできない。

--- arrays/note.js
+++ arrays/note.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{}, {
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 /* console.log(notes.pop())
 notes.push('My new note')
@@ -8,14 +17,14 @@ notes.unshift('My first note') */
 
 // notes.splice(1, 1, 'This is the new second item')
 
-notes[2] = 'This is now the new note 3'
+/* notes[2] = 'This is now the new note 3'
 
 notes.forEach(function (item, index) {
     console.log(index)
     console.log(item)
-})
+}) */
 
 console.log(notes.length)
 console.log(notes)
 
-console.log(notes.indexOf('note 2'))
+console.log(notes.indexOf({}))
