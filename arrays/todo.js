7277ba93a3665eff84100ad55ef2e00b11dd74a1
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-17 14:56:54 +0900

    [update]配列の検索２
    模範解答

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,9 +1,34 @@
-let todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+const todos = [{
+    text: 'Order cat food',
+    completed: true
+}, {
+    text: 'Clean kitchen',
+    completed: false
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
+
+const deleteTodo = function (todos, todoText) {
+    const index = todos.findIndex(function (todo) {
+        return todo.text.toLowerCase() === todoText.toLowerCase()
+    })
+    
+    if (index > -1) {
+        todos.splice(index, 1)
+    }
+}
 
 // 1. Convert array to array of objects -> text, completed
 // 2. Create function to remove a todo by text value
 
-const deleteTodo = function (todos, todoText) {
+/* const deleteTodo = function (todos, todoText) {
     const index = todos.findIndex(function (todo) {
         return todo.text.toLowerCase() === todoText.toLowerCase()
     })
@@ -18,6 +43,7 @@ todos.forEach(function (value) {
     })
 })
 todos = todos_hash
+*/
 
 deleteTodo(todos, 'buy food')
 console.log(todos)
