4a2ffe1841226ceb10f7d405b7b4744513d55248
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-18 13:28:26 +0900

    [update]filterの学習
    チャレンジ問題を自分なりに

--- arrays/todo.js
+++ arrays/todo.js
@@ -25,25 +25,13 @@ const deleteTodo = function (todos, todoText) {
     }
 }
 
-// 1. Convert array to array of objects -> text, completed
-// 2. Create function to remove a todo by text value
-
-/* const deleteTodo = function (todos, todoText) {
-    const index = todos.findIndex(function (todo) {
-        return todo.text.toLowerCase() === todoText.toLowerCase()
+const getThingsToDo = function (todos) {
+    return todos.filter(function (todo) {
+        return !todo.completed
     })
-    todos.splice(index, 1)
 }
 
-const todos_hash = []
-todos.forEach(function (value) {
-    todos_hash.push({
-        text: value,
-        completed: false
-    })
-})
-todos = todos_hash
-*/
+console.log(getThingsToDo(todos))
 
-deleteTodo(todos, '!!buy food')
-console.log(todos)
+/* deleteTodo(todos, '!!buy food')
+console.log(todos) */
