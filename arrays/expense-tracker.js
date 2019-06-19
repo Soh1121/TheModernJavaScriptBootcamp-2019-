028c98d4c96acecf8ad5f1ce9a24b1688f8fd93b
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-06-19 10:01:04 +0900

    [update]チャレンジ問題
    模範解答

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -8,17 +8,32 @@ const account = {
         })
     },
     getAccountSummary: function () {
+        let totalExpenses = 0
+
+        this.expenses.forEach(function (expense) {
+            totalExpenses = totalExpenses + expense.amount
+        })
+
+        return `${this.name} has $${totalExpenses} in expenses.`
+    }
+    /* addExpense: function (description, amount) {
+        this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    getAccountSummary: function () {
         let total = 0
         this.expenses.forEach(function (expense) {
             total += expense.amount
         })
-        return `${this.name} has $${total} in expenses`
-    }
+        return `${this.name} has $${total} in expenses.`
+     }*/
 }
 
 // Expense -> description, amount
 // addExpense -> description, amount
-// getAccountSummary -> total up all expenses -> Andrew Mead has $1250 in expenses
+// getAccountSummary -> total up all expenses -> Andrew Mead has $1250 in expenses.
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
