ff16c1614b180724d4fc03b1a7749194615173ea
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-06-19 10:14:17 +0900

    [update]チャレンジ問題
    模範解答

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,23 +1,37 @@
 const account = {
     name: 'Andrew Mead',
     expenses: [],
-    incomes: [],
-    /* addExpense: function (description, amount) {
+    income: [],
+    addExpense: function (description, amount) {
         this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    addIncome: function (description, amount) {
+        this.income.push({
             description: description,
             amount: amount
         })
     },
     getAccountSummary: function () {
         let totalExpenses = 0
+        let totalIncome = 0
+        let accountBalance = 0
 
         this.expenses.forEach(function (expense) {
             totalExpenses = totalExpenses + expense.amount
         })
 
-        return `${this.name} has $${totalExpenses} in expenses.`
-    } */
-    addExpense: function (description, amount) {
+        this.income.forEach(function (income) {
+            totalIncome = totalIncome + income.amount
+        })
+
+        accountBalance = totalIncome - totalExpenses
+
+        return `${this.name} has a balance of $${accountBalance}. $${totalIncome} in income. $${totalExpenses} in expenses.`
+    }
+    /* addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
@@ -38,8 +52,8 @@ const account = {
         this.incomes.forEach(function (income) {
             totalIncomes += income.amount
         })
-        return `${this.name} has a blance of $${totalIncomes - totalExpenses}. $${totalIncomes} in income. $${totalExpenses} in expenses.`
-     }
+        return `${this.name} has a balance of $${totalIncomes - totalExpenses}. $${totalIncomes} in income. $${totalExpenses} in expenses.`
+     } */
 }
 
 // 1. add income array to account
