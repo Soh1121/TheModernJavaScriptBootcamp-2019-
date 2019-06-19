e5a4bfad94c2f856db18710f48d6c30b8fe9d84b
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-06-19 10:08:40 +0900

    [update]チャレンジ問題
    チャレンジ問題を自分なりに

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,7 +1,8 @@
 const account = {
     name: 'Andrew Mead',
     expenses: [],
-    addExpense: function (description, amount) {
+    incomes: [],
+    /* addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
@@ -15,26 +16,40 @@ const account = {
         })
 
         return `${this.name} has $${totalExpenses} in expenses.`
-    }
-    /* addExpense: function (description, amount) {
+    } */
+    addExpense: function (description, amount) {
         this.expenses.push({
             description: description,
             amount: amount
         })
     },
-    getAccountSummary: function () {
-        let total = 0
-        this.expenses.forEach(function (expense) {
-            total += expense.amount
+     addIncome: function (description, amount) {
+        this.incomes.push({
+            description: description,
+            amount: amount
         })
-        return `${this.name} has $${total} in expenses.`
-     }*/
+     },
+     getAccountSummary: function () {
+        let totalExpenses = 0
+        this.expenses.forEach(function (expense) {
+            totalExpenses += expense.amount
+        })
+        let totalIncomes = 0
+        this.incomes.forEach(function (income) {
+            totalIncomes += income.amount
+        })
+        return `${this.name} has a blance of $${totalIncomes - totalExpenses}. $${totalIncomes} in income. $${totalExpenses} in expenses.`
+     }
 }
 
-// Expense -> description, amount
-// addExpense -> description, amount
-// getAccountSummary -> total up all expenses -> Andrew Mead has $1250 in expenses.
+// 1. add income array to account
+// 2. addIncome method -> description, amount
+// 3. Tweak getAccountSummary
+
+// Andrew Mead has a blance of $10. $100 in income. $90 in expenses.
+
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
