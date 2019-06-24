10f191a5bb4eb84c2c83e62e7a91b99e40b73b0d
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-24 13:01:24 +0900

    [update]DOM操作の学習
    pタグをすべて削除

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,5 +1,12 @@
 // DOM - Document Object Model
 
 // Query and remove
-const p = document.querySelector('p')
-p.remove()
+/* const p = document.querySelector('p')
+p.remove() */
+
+// Query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+    p.remove()
+})
