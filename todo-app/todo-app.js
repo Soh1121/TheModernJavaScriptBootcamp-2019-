c395af56c6c8fbc14d5d1676486042a2fc53bd7d
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-06-25 16:10:57 +0900

    [update]新たな要素をJavaScriptから追加
    サマリー表示を失念していたため追記

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -22,7 +22,10 @@ const leftTodos = todos.filter(function (todo) {
     return !todo.completed
 })
 
-console.log(leftTodos)
+const summary = document.createElement('h2')
+summary.textContent = `You have ${leftTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
 leftTodos.forEach(function (todo) {
     const newParagraph = document.createElement('p')
     newParagraph.textContent = todo.text
