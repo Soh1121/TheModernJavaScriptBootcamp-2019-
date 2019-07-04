dd55ebc8ade42376c29416278cb99fb53815c082
Soh1121 <satou.shg@gmail.com>
2019-07-04 22:25:21 +0900

    [update]フィルター機能の実装
    フィルター機能の動作をコンソールで確認

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,6 +9,19 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    console.log(filteredNotes)
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
@@ -20,5 +33,6 @@ document.querySelector('#remove-all').addEventListener('click', function () {
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
 })
