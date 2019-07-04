0a44f2daaeca23eb75076ec4f6e518df2d9d0b4d
Soh1121 <satou.shg@gmail.com>
2019-07-04 22:33:33 +0900

    [update]フィルター機能の実装
    タグの中身をいったん空にすることで検索結果のみを出力する形に

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -17,11 +17,13 @@ const renderNotes = function (notes, filters) {
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
+
+    document.querySelector('#notes').innerHTML = ''
     
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
         noteEl.textContent = note.title
-        document.querySelector('body').appendChild(noteEl)
+        document.querySelector('#notes').appendChild(noteEl)
     })
 }
 
