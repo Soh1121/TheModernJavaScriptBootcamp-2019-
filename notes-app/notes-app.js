200052ee5aff9f7f9c55d6e48978db8d1052f3b8
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-07-08 13:16:29 +0900

    [update]より詳しくフォームを学習
    インプットボックスの値へアクセス

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -40,4 +40,5 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 
 document.querySelector('#name-form').addEventListener('submit', function (e) {
     e.preventDefault()
+    console.log(e.target.elements.firstName.value)
 })
