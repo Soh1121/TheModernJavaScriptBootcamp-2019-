b088fa4f93473ed693324bf78489948811eaaf62
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-07-17 09:15:35 +0900

    [update]デバッグ方法の学習
    チャレンジ問題を自分なりに

--- todo-app/todo-functions-ans.js
+++ todo-app/todo-functions-ans.js
@@ -16,17 +16,13 @@ const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-
+        debugger
         return searchTextMatch && hideCompletedMatch
     })
-
-    debugger
 
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.completed
     })
-
-    debugger
 
     document.querySelector('#todos').innerHTML = ''
     document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
