03c0c117bbd116354bb889f62883f9175887548a
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-07-24 12:36:30 +0900

    [update]ノートの編集ページの作成
    チャレンジ問題を自分なりに

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -7,3 +7,33 @@ const note = notes.find(function (note) {
 if (note === undefined) {
     location.assign('./index.html')
 }
+
+document.querySelector('#note-title').value = note.title
+document.querySelector('#note-body').value = note.body
+
+document.querySelector('#note-title').addEventListener('input', function (e) {
+    const noteTitle = e.target.value
+    if (noteTitle.length > 0) {
+        note.title = noteTitle
+        saveNotes(notes)
+    } else {
+        note.title = ""
+        saveNotes(notes)
+    }
+})
+
+document.querySelector('#note-body').addEventListener('input', function (e) {
+    const noteBody = e.target.value
+    if (noteBody.length > 0) {
+        note.body = noteBody
+        saveNotes(notes)
+    } else {
+        note.body = ""
+        saveNotes(notes)
+    }
+})
+
+document.querySelector('#remove-note').addEventListener('submit', function (e) {
+    removeNote(note.id)
+    location.assign('./index.html')
+})
--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -14,22 +14,23 @@ const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+// Remove a note from the list
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+    saveNotes(notes)
+}
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
     const textEl = document.createElement('a')
     const button = document.createElement('button')
-
-    // Remove a note from the list
-    const removeNote = function (id) {
-        const noteIndex = notes.findIndex(function (note) {
-            return note.id === id
-        })
-
-        if (noteIndex > -1) {
-            notes.splice(noteIndex, 1)
-        }
-    }
 
     // Setup the remove note button
     button.textContent = 'x'
