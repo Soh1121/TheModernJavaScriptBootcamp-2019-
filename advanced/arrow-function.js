e7a542c83b1859bb3164ce3e9643420d8b2d2fab
Soh1121 <satou.shg@gmail.com>
2019-07-31 23:35:14 +0900

    [update]arrow関数の学習
    arrow関数に変更するとこうなる

--- advanced/arrow-function.js
+++ advanced/arrow-function.js
@@ -17,8 +17,10 @@ const people = [{
     age: 22
 }]
 
-const under30 = people.filter(function (person) {
+/* const under30 = people.filter(function (person) {
     return person.age < 30
-})
+}) */
+
+const under30 = people.filter((person) => person.age < 30)
 
 console.log(under30)
