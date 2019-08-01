67e855e2ecf260e7338d93d549818b8280543fb0
Soh1121 <satou.shg@gmail.com>
2019-08-01 14:38:58 +0900

    [update]arrow関数の学習
    メソッドの場合

--- advanced/arrow-function-2.js
+++ advanced/arrow-function-2.js
@@ -3,3 +3,12 @@ const add = () => {
 }
 
 console.log(add(11, 22, 33, 44))
+
+const car = {
+    color: 'Red',
+    getSummary: function () {
+        return `The car is ${this.color}`
+    }
+}
+
+console.log(car.getSummary())
