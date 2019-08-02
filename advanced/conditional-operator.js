192611d2a2d813562077d6c6e7ce07d691119d32
Soh1121 <satou.shg@gmail.com>
2019-08-02 10:40:03 +0900

    [update]条件演算子の学習
    条件演算子で表記

--- advanced/conditional-operator.js
+++ advanced/conditional-operator.js
@@ -1,10 +1,14 @@
 const myAge = 27
 let message
 
+message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
+
+/*
 if (myAge >= 18) {
     message = 'You can vote!'
 } else {
     message = 'You cannot vote.'
 }
+*/
 
 console.log(message)
