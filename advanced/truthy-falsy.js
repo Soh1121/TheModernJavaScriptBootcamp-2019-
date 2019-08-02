9438fc652f1f9479d87c063542090ee32ac3c05b
Soh1121 <satou.shg@gmail.com>
2019-08-02 11:18:38 +0900

    [update]TruthyとFalsy
    空の配列はTruthy

--- advanced/truthy-falsy.js
+++ advanced/truthy-falsy.js
@@ -1,11 +1,11 @@
-const products = ['some item']
+const products = []
 const product = products[0]
 
 // Truthy - Values that resolve to true in boolean context
 // Falsy - Values that resolve to false in boolean context
 // Falsy values - false, 0, empty string, null, undefined
 
-if ('') {
+if (products) {
     console.log('Product found')
 } else {
     console.log('Product not found')
