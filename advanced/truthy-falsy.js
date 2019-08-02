f27061dfd7d227279068c995dbccda7d502164fb
Soh1121 <satou.shg@gmail.com>
2019-08-02 11:17:48 +0900

    [update]TruthyとFalsy
    空の文字列はFalsy

--- advanced/truthy-falsy.js
+++ advanced/truthy-falsy.js
@@ -5,7 +5,7 @@ const product = products[0]
 // Falsy - Values that resolve to false in boolean context
 // Falsy values - false, 0, empty string, null, undefined
 
-if (0) {
+if ('') {
     console.log('Product found')
 } else {
     console.log('Product not found')
