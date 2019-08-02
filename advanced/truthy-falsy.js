4aa8caa7125d17e8ece7a469381ea5f5ee519a2a
Soh1121 <satou.shg@gmail.com>
2019-08-02 11:08:32 +0900

    [update]TruthyとFalsy
    Truthyの例

---
+++ advanced/truthy-falsy.js
@@ -0,0 +1,11 @@
+const products = ['some item']
+const product = products[0]
+
+// Truthy - Values that resolve to true in boolean context
+// Falsy - Values that resolve to false in boolean context
+
+if ('testing') {
+    console.log('Product found')
+} else {
+    console.log('Product not found')
+}
