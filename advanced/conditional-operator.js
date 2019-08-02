62233b31472728073d56fac03d7b1357a9f747c5
Soh1121 <satou.shg@gmail.com>
2019-08-02 10:44:09 +0900

    [update]条件演算子の学習
    関数も条件演算子に含めることが可能

--- advanced/conditional-operator.js
+++ advanced/conditional-operator.js
@@ -1,12 +1,15 @@
+/*
 const myAge = 27
 const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote.'
-
-/*
-if (myAge >= 18) {
-    message = 'You can vote!'
-} else {
-    message = 'You cannot vote.'
-}
+console.log(message)
 */
 
-console.log(message)
+const myAge = 27
+const showPage = () => {
+    console.log('Showing the page')
+}
+const showErrorPage = () => {
+    console.log('Showing the error page')
+}
+
+myAge >= 21 ? showPage() : showErrorPage()
