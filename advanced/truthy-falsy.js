d43e2a18de8825c9ef5a75880f57ac551f4787e8
Soh1121 <satou.shg@gmail.com>
2019-08-02 11:16:00 +0900

    [update]TruthyとFalsy
    Falsyの値の紹介とundefinedがFalsyとなる確認

--- advanced/truthy-falsy.js
+++ advanced/truthy-falsy.js
@@ -3,8 +3,9 @@ const product = products[0]
 
 // Truthy - Values that resolve to true in boolean context
 // Falsy - Values that resolve to false in boolean context
+// Falsy values - false, 0, empty string, null, undefined
 
-if ('testing') {
+if (undefined) {
     console.log('Product found')
 } else {
     console.log('Product not found')
