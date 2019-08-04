366ec9245ed6e9e789ac349426de561db2e0eb7b
Soh1121 <satou.shg@gmail.com>
2019-08-04 21:06:38 +0900

    [update]型の学習
    booleanは加算に入れると数値（true=1, flase=0）として扱われる

--- advanced/type-coercion.js
+++ advanced/type-coercion.js
@@ -1,8 +1,6 @@
 // Type Coercion - a string, a number, a boolean
 
-console.log('5' + 5)
-console.log('5' - 5)
-console.log('5' === 5)
-
-const type = typeof {}
+const value = true + 12
+const type = typeof value
 console.log(type)
+console.log(value)
