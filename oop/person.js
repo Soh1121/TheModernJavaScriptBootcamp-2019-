f5efef9812f53bd4c6c125efaacea378b3990878
Soh1121 <satou.shg@gmail.com>
2019-08-06 13:46:15 +0900

    [update]コンストラクタ関数
    セットする値を追加

--- oop/person.js
+++ oop/person.js
@@ -1,7 +1,9 @@
-const Person = function (firstName) {
+const Person = function (firstName, lastName, age) {
     this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
 }
 
-const me = new Person('Andrew')
+const me = new Person('Andrew', 'Mead', 27)
 
 console.log(me)
