8f493deb2b368e5d51b2c1e0b9c0ec78c138cca9
Soh1121 <satou.shg@gmail.com>
2019-08-06 13:48:47 +0900

    [update]コンストラクタ関数
    インスタンスの増やし方

--- oop/person.js
+++ oop/person.js
@@ -5,5 +5,7 @@ const Person = function (firstName, lastName, age) {
 }
 
 const me = new Person('Andrew', 'Mead', 27)
-me.firstName = 'Jen'
 console.log(me)
+
+const person2 = new Person('Clancey', 'Turner', 51)
+console.log(person2)
