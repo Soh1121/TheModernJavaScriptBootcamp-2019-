b80a75139c2c464f9aef7b0f1070eebae5068c9c
Soh1121 <satou.shg@gmail.com>
2019-08-12 23:12:06 +0900

    [update]prototypeの学習
    prototypeの基本

--- oop/person.js
+++ oop/person.js
@@ -1,11 +1,17 @@
+// Prototypal Inheritance
+
 const Person = function (firstName, lastName, age) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
 }
 
+Person.prototype.getBio = function () {
+    return `${this.firstName} is ${this.age}.`
+}
+
 const me = new Person('Andrew', 'Mead', 27)
-console.log(me)
+console.log(me.getBio())
 
 const person2 = new Person('Clancey', 'Turner', 51)
-console.log(person2)
+console.log(person2.getBio())
