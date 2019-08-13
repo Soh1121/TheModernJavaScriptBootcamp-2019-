5985c5df9f200622b8dc34c3260dfa34b2d86810
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-13 11:40:14 +0900

    [update]プリミティブとオブジェクト
    オブジェクトを確認

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,8 @@
+// product --> Object.prototype --> null
 const product = {
     name: 'Influence'
 }
 
 // hasOwnProperty
-console.log(product.hasOwnProperty('price'))
+console.log(product.hasOwnProperty('hasOwnProperty'))
+console.log(product)
