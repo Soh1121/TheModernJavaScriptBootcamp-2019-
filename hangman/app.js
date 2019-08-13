61c7884f2723bea374523f21824ab935e47eb8d5
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-13 11:43:38 +0900

    [update]プリミティブとオブジェクト
    hasOwnPropertyを継承

--- hangman/app.js
+++ hangman/app.js
@@ -3,6 +3,8 @@ const product = {
     name: 'Influence'
 }
 
+Object.prototype.hasOwnProperty = () => 'This is the new function'
+
 // hasOwnProperty
 console.log(product.hasOwnProperty('hasOwnProperty'))
 console.log(product)
