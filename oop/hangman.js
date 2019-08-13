4daf1e67d6858a60155c069d16ac5dc62a94c1fd
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-13 11:01:29 +0900

    [update]prototypeのチャレンジ
    模範解答

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,33 +1,25 @@
-// 1. Set up the word instance property as an array of lower case letters
-// 2. Set up another instance property to store guessed letters
-// 3. Create a method that gives you the word puzzle back
-
-// No guesses? -> ***
-// Guessed "c", "b", and "t"? -> c*t
-
-const Hangman = function (word, remainingGuesses, guessedLetters = []) {
-    this.word = word
-    this.lowerCaseLetters = word.toLowerCase().split('')
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = guessedLetters
+    this.guessedLetters = ['c', 'e']
 }
 
 Hangman.prototype.getPuzzle = function () {
-    let result =''
-    this.lowerCaseLetters.forEach((letter) => {
-        const letterIndex = this.guessedLetters.findIndex((guessedLetter) => guessedLetter === letter)
-
-        if (letterIndex > -1) {
-            result += letter
+    let puzzle =''
+    
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
         } else {
-            result += '*'
+            puzzle += '*'
         }
     })
-    return result
+
+    return puzzle
 }
 
-const game1 = new Hangman('Cat', 2, ['c', 'b', 't'])
+const game1 = new Hangman('Cat', 2)
 console.log(game1.getPuzzle())
 
-const game2 = new Hangman('New Jersey', 4, ['c', 'b', 't'])
+const game2 = new Hangman('New Jersey', 4)
 console.log(game2.getPuzzle())
