bd6fb6f3d7faf07ce2a7921bbf2aadb73a73ba59
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-13 11:25:09 +0900

    [update]prototypeの継承
    次に向けて整形

--- oop/person.js
+++ oop/person.js
@@ -24,11 +24,6 @@ Person.prototype.setName = function (fullName) {
 }
 
 const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
-
-me.getBio = function () {
-    return 'This is fake!'
-}
-
 me.setName('Alexis Turner')
 console.log(me.getBio())
 
