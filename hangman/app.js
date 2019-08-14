ef34108c9af0368545f747ab4aa666e31113d286
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-14 10:51:05 +0900

    [update]HangmanChallenge3
    チャレンジ問題を自分なりに

--- hangman/app.js
+++ hangman/app.js
@@ -12,3 +12,47 @@ console.log(product)
 
 const otherProduct = new String('Phone')
 console.log(otherProduct)
+
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.guessedLetters = []
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle =''
+    
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
+}
+
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+    if (isUnique) {
+        this.guessedLetters.push(guess)
+    }
+
+    if (isUnique && isBadGuess) {
+        this.remainingGuesses--
+    }
+}
+
+Hangman.prototype.renderPuzzle = function () {
+    const puzzleEl = document.querySelector('#puzzle')
+    const textEl = document.createElement('p')
+    const remainingEl = document.createElement('p')
+    textEl.textContent = this.getPuzzle()
+    remainingEl.textContent= this.remainingGuesses
+    puzzleEl.appendChild(textEl)
+    puzzleEl.appendChild(remainingEl)
+}
