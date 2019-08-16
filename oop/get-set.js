72d61593a95673a824b71e106b0183cb15399e41
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 14:19:07 +0900

    [update]ゲッターとセッター
    セッターの例

--- oop/get-set.js
+++ oop/get-set.js
@@ -1,9 +1,12 @@
 const data = {
     get location() {
         return 'This is a test'
+    },
+    set location(value) {
+        this._location = value.trim()
     }
 }
 
 // code that uses the data object
 data.location = '  Philadelphia    '
-console.log(data.location)
+console.log(data)
