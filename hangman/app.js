243af32933736d9635595dbc379c1d95746c92c4
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 14:47:40 +0900

    [update]HangmanGameの改修
    スペースを含む場合文字を当てても終了しない問題を改修

--- hangman/app.js
+++ hangman/app.js
@@ -1,6 +1,6 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Cas', 2)
+const game1 = new Hangman('Car Parts', 2)
 
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
