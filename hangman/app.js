cae9601626a27e63ff92d2da920aa4e7b6ba03c3
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 15:06:07 +0900

    [update]HTTPリクエスト
    httpリクエストの基本

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,6 @@
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Car Parts', 2)
@@ -12,3 +15,9 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
