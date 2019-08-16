d8fdba07a787ae6b0d54c7956a381f2a9e294247
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 15:37:35 +0900

    [update]HTTPヘッダーとエラー
     アロー関数に変更

--- hangman/app.js
+++ hangman/app.js
@@ -9,7 +9,7 @@ puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 console.log(game1.status)
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
