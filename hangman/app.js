41082ddc400ad5a29dea73a2d03ca4daa91f1dc5
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 15:42:08 +0900

    [update]HTTPヘッダーとエラー
    ヘッダーを確認するように変更

--- hangman/app.js
+++ hangman/app.js
@@ -20,7 +20,7 @@ window.addEventListener('keypress', (e) => {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
     }
