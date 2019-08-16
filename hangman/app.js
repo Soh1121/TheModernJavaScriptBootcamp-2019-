4897c4e00fef8c4abddfb1abd118464e642bc940
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-16 15:45:11 +0900

    [update]HTTPヘッダーとエラー
    エラー処理を追加

--- hangman/app.js
+++ hangman/app.js
@@ -23,8 +23,10 @@ request.addEventListener('readystatechange', (e) => {
     if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState=== 4) {
+        console.log('And error has taken place')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?key=value')
 request.send()
