34128585e7fdf8dfa62062f5179a560d8b46f4c7
Soh1121 <satou.shg@gmail.com>
2019-08-17 23:15:47 +0900

    [update]コールバックの抽象化
    requestを別ファイルに移動

--- hangman/app.js
+++ hangman/app.js
@@ -17,20 +17,8 @@ const puzzle = getPuzzle()
 console.log(puzzle)
 
 // Making an HTTP request
-/* const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if (e.target.readyState === 4) {
-        console.log('And error has taken place')
-    }
-})
-
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
-
+/*
 const countryCode = "US"
 const countryRequest = new XMLHttpRequest()
 
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,3 +1,16 @@
 const getPuzzle = () => {
-    return 'My new puzzle'
+    const request = new XMLHttpRequest()
+
+    request.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            console.log(data)
+            return data
+        } else if (e.target.readyState === 4) {
+            console.log('And error has taken place')
+        }
+    })
+
+    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+    request.send()
 }
