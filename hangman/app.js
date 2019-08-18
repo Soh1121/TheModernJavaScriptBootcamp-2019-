267087f51fd895182b19cf3d3b177fccdcce2df3
Soh1121 <satou.shg@gmail.com>
2019-08-18 23:32:42 +0900

    [update]コールバック関数のチャレンジ問題
    自分なりに

--- hangman/app.js
+++ hangman/app.js
@@ -13,7 +13,7 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = game1.statusMessage
 })
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
@@ -21,22 +21,14 @@ getPuzzle((error, puzzle) => {
     }
 })
 
-// Making an HTTP request
-
-/*
-const countryCode = "US"
-const countryRequest = new XMLHttpRequest()
-
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.alpha2Code === countryCode)
-        console.log(country.name)
-    } else if (e.target.readyState === 4) {
-        console.log('Unable to fetch data')
+// 1. Create a new function for getting country details
+// 2. Call it with two arguments: country code, the callback function
+// 3. Make the HTTP request and call the callback with country information
+// 4. Use the callback to print the country name
+getCountry("US", (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(country)
     }
 })
-
-countryRequest.open('GET', 'https://restcountries.eu/rest/v2/all')
-countryRequest.send()
- */
