d4999e982547f7f1b4dba4f6e0c1baba0c736ed1
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-08-19 13:00:14 +0900

    [update]別なクロージャの例を追加
    引数を持つ関数の場合　

--- functions/closures.js
+++ functions/closures.js
@@ -20,3 +20,13 @@ counter.decrement()
 counter.decrement()
 counter.count = 0
 console.log(counter.get())
+
+// Adder
+const createAdder = (a) => {
+    return (b) => {
+        return a + b
+    }
+}
+const add10 = createAdder(10)
+console.log(add10(-2))
+console.log(add10(20))
