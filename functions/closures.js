edc7fddbdf6e46be0b84eaf980c0664721c7197f
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-08-19 13:07:00 +0900

    [update]チップを計算する処理を追加
    チャレンジ問題を自分なりに

--- functions/closures.js
+++ functions/closures.js
@@ -22,6 +22,7 @@ counter.count = 0
 console.log(counter.get())
 
 // Adder
+const add = (a, b) => a + b
 const createAdder = (a) => {
     return (b) => {
         return a + b
@@ -32,3 +33,19 @@ console.log(add10(-2))
 console.log(add10(20))
 const add100 = createAdder(100)
 console.log(add100(-90))
+
+// Tipper
+
+// 1. Create createTipper which takes in the base tip (.15 for 15% tip)
+// 2. Set it up to return a function that takes in the bill amount
+// 3. Call createTipper to generate a few functions for different percentages
+// 4. Use the generated functions to calculate tips and print them
+
+const createTipper = (baseTip) => {
+    return (billAmount) = (amount) => {
+        return amount * (1 + baseTip)
+    }
+}
+const tip15 = createTipper(.15)
+console.log(tip15(100))
+console.log(tip15(200))
