be27f4232b6f740a96d19e8dec1214313ddd5399
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-19 13:41:08 +0900

    [update]Promiseオブジェクトのエラー例
    警告が出る

--- functions/promises.js
+++ functions/promises.js
@@ -1,13 +1,13 @@
 // Callback
 const getDataCallback = (callback) => {
     setTimeout(() => {
-        callback(undefined, 'This is the data')
+        callback('This is my callback error', undefined)
     }, 2000)
 }
 
 getDataCallback((err, data) => {
     if (err) {
-
+        console.log(err)
     } else {
         console.log(data)
     }
@@ -16,7 +16,8 @@ getDataCallback((err, data) => {
 // Promise
 const myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve('This is the promise data')
+        // resolve('This is the promise data')
+        reject('This is my promise error')
     }, 2000)
 })
 
