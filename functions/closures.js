35d470d69b27beff746a0f38bfe08e9fdbecaeaf
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-19 13:10:51 +0900

    [update]チップ額のみの計算に変更
    模範解答

--- functions/closures.js
+++ functions/closures.js
@@ -35,17 +35,12 @@ const add100 = createAdder(100)
 console.log(add100(-90))
 
 // Tipper
-
-// 1. Create createTipper which takes in the base tip (.15 for 15% tip)
-// 2. Set it up to return a function that takes in the bill amount
-// 3. Call createTipper to generate a few functions for different percentages
-// 4. Use the generated functions to calculate tips and print them
-
 const createTipper = (baseTip) => {
-    return (billAmount) = (amount) => {
-        return amount * (1 + baseTip)
+    return (amount) => {
+        return baseTip * amount
     }
 }
-const tip15 = createTipper(.15)
-console.log(tip15(100))
-console.log(tip15(200))
+const tip20 = createTipper(.2)
+const tip30 = createTipper(.3)
+console.log(tip20(100))
+console.log(tip30(100))
