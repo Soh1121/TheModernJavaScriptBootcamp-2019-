a1b180fa4088fac70fe73a0fe14cc779b18361ce
SoichiSato <soichi@kenki10B-02.nsv.nhk>
2019-08-22 13:34:32 +0900

    [update]入力文字列を大文字にして！を付けて返す関数をモジュール化するチャレンジ問題
    自分なりに

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,6 +1,14 @@
-// import './utilities.js'
 import { add, name } from './utilities'
+import { scream } from './scream'
 
 console.log('index.js')
 console.log(add(32, 1))
 console.log(name)
+
+// 1. Create new file called scream.js
+// 2. Export a "scream" function that takes a string
+//    a. Convert that string to upper case
+//    b. Add "!" onto the end
+// 3. Import scream into index.js add use it
+
+console.log(scream('scream'))
