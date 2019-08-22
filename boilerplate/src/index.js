e07bfa15fca8ce4fbed27be25b2e7ba360f8a595
Soh1121 <satou.shg@gmail.com>
2019-08-23 08:38:28 +0900

    [update]export defaultを作成

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,6 +1,7 @@
-import { add, name } from './utilities'
+import otherSquare, { add, name } from './utilities'
 import { scream } from './scream'
 
 console.log('index.js')
 console.log(add(32, 1))
 console.log(scream(name))
+console.log(otherSquare(10))
--- boilerplate/src/utilities.js
+++ boilerplate/src/utilities.js
@@ -1,5 +1,9 @@
-console.log('utilities.js')
+// Named export
+// Default export
 
 export const add = (a, b) => a + b
 
 export const name = 'Andrew'
+
+const square = (x) => x * x
+export default square
