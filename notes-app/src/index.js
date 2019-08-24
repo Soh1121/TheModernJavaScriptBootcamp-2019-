908e706f515270f27b6e9f34953e5bb634bf5ba7
Soh1121 <satou.shg@gmail.com>
2019-08-24 11:09:29 +0900

    [update]データ削除を移行

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,5 +1,6 @@
-import { getNotes, createNote } from './notes'
+import { getNotes, createNote, removeNote } from './notes'
 
-console.log(getNotes())
-createNote()
+// console.log(getNotes())
+// createNote()
+removeNote('887191c3-f157-44f8-a283-2c86026c4ad5')
 console.log(getNotes())
