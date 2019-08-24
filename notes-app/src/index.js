d15689b843c5e8d3cf99db31e09f9b8e1089afd2
Soh1121 <satou.shg@gmail.com>
2019-08-24 11:18:41 +0900

    [update]ソート部分とデータ編集部分を移行

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -1,6 +1,11 @@
-import { getNotes, createNote, removeNote } from './notes'
+import { getNotes, createNote, removeNote, updateNote } from './notes'
 
 // console.log(getNotes())
 // createNote()
-removeNote('887191c3-f157-44f8-a283-2c86026c4ad5')
+//removeNote('887191c3-f157-44f8-a283-2c86026c4ad5')
+
+updateNote('4631ab1f-4f9c-41d3-8487-bde704a2747c', {
+    title: 'My note title',
+    body: 'This is the body for my note.'
+})
 console.log(getNotes())
