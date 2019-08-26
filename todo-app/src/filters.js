ed941c0ab23ff11f6e37bbac75a104067b784a0c
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-26 21:44:24 +0900

    [update]フィルターの値の取得と値のセットを自分なりに

--- todo-app/src/filters.js
+++ todo-app/src/filters.js
@@ -7,9 +7,19 @@ const filters = {
 // getFilters
 // Arguments: none
 // Return value: filters object
+const getFilters = () => filters
 
 // setFilters
 // Arguments: updates object with optional searchText or hideCompleted
 // Return value: none
+const setFilters = (updates) => {
+    if (typeof updates.searchText === 'string') {
+        filters.searchText = updates.searchText
+    }
+    if (filters.hideCompleted !== updates.hideCompleted) {
+        filters.hideCompleted = updates.hideCompleted
+    }
+}
 
 // Make sure to set up the exports
+export { getFilters, setFilters}
