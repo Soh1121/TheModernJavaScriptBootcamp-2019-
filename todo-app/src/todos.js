1f0f14fb88496024735ddbba1abd48994258d84a
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-27 11:28:09 +0900

    [update]todos.jsを自分なりに

--- todo-app/src/todos.js
+++ todo-app/src/todos.js
@@ -1,28 +1,72 @@
+import uuidv4 fro 'uuid/v4'
+
 // Setup the empty todos array
 let todos = []
 
 // loadTodos
 // Arguments: none
 // Return value: none
+const loadTodos = () => {
+    const todosJSON = localStorage.getItem('todos')
+
+    try {
+        return todosJSON ? JSON.parse(todosJSON) : []
+    } catch (e) {
+        return []
+    }
+}
 
 // saveTodos
 // Arguments: none
 // Return value: none
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
 
 // getTodos
 // Arguments: none
 // Return value: todos array
+const getTodos = () => todos
 
 // createTodo
 // Arguments: todo text
 // Return value: none
+const createTodo = (text) => {
+    if (text.length > 0) {
+        todos.push({
+            id: uuidv4(),
+            text: text,
+            completed: false
+        })
+        saveTodos()
+    }
+}
 
 // removeTodo
 // Arguments: id of todo to remove
 // Return value: none
+const removeTodo = (id) => {
+    const todoIndex = todos.findIndex((todo) => todo.id === id)
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+        saveTodos()
+    }
+}
 
 // toggleTodo
 // Arguments: id of todo to toggle
 // Return value: none
+const toggleTodo = (id) => {
+    const todo = todos.find((todo) => todo.id === id)
+
+    if (todo) {
+        todo.completed = !todo.completed
+        saveTodos()
+    }
+}
+
+todos = loadTodos()
 
 // Make sure to call loadTodos and setup the exports
+export { getTodos, createTodo, removeTodo, toggleTodo }
