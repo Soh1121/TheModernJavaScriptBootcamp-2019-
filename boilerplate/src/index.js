b9c41e6991e63496c6e1d21035a6d30b4deffd60
SoichiSato <soichi@Soichi-iMac.nsv.nhk>
2019-08-27 16:42:15 +0900

    [update]引数の数を動的に動的に変更
    ...でリスト化

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,5 +1,8 @@
-const calculateAverage = (numOne, numTwo) => {
-    return (numOne + numTwo) / 2
+const calculateAverage = (...numbers) => {
+    // return (numOne + numTwo) / 2
+    let sum = 0
+    numbers.forEach((num) => sum += num)
+    return sum / numbers.length
 }
 
-console.log(calculateAverage(0, 100))
+console.log(calculateAverage(0, 100, 88, 64))
