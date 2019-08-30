0fad20b1bef0cfa9fae1ffe2018f3428bafe7536
Soh1121 <satou.shg@gmail.com>
2019-08-30 23:46:51 +0900

    [update]配列のスプレッド構文

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -15,8 +15,7 @@ console.log(details)
 console.log(others)
 
 const age = [65, 0, 13]
-const [firstAge, secondAge, , lastAge = 123] = age
+const [firstAge, ...otherAges] = age
 
 console.log(firstAge)
-console.log(secondAge)
-console.log(lastAge)
+console.log(otherAges)
