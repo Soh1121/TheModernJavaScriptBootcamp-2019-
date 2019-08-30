9699b17a22ed20fe29f9ef8f47bd89905c26e3b7
Soh1121 <satou.shg@gmail.com>
2019-08-30 23:38:31 +0900

    [update]destructuringの準備

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -1,47 +1,11 @@
-const printTeam = (teamName, coach, ...players) => {
-    console.log(`Team: ${teamName}`)
-    console.log(`Coach: ${coach}`)
-    console.log(`Players: ${players.join(', ')}`)
+const todo = {
+    id: 'asdfpoijwermasdf',
+    text: 'Pay the bills',
+    completed: false
 }
 
-const team = {
-    name: 'Libery',
-    coach: 'Casey Penn',
-    players: ['Marge', 'Aiden', 'Herbert', 'Sherry']
-}
-printTeam(team.name, team.coach, ...team.players)
+const text = todo.text
+const completed = todo.completed
 
-let cities = ['Barcelona', 'Cape Town', 'Bordeaux']
-cities = [...cities, 'Santiago']
-console.log(cities)
-
-let house = {
-    bedrooms: 2,
-    bathrooms: 1.5,
-    yearBuilt: 2017
-}
-let newHouse = {
-    basement: true,
-    bedrooms: 3,
-    ...house
-}
-
-console.log(house)
-console.log(newHouse)
-
-// Create a person object with name and age
-// Create a location object with city and country
-// Create a new overview object and use the spread operator to add all four properties
-const person = {
-    name: 'Andrew',
-    year: 27
-}
-const location = {
-    city: 'Philadelphia',
-    country: 'USA'
-}
-const overview = {
-    ...person,
-    ...location
-}
-console.log(overview)
+console.log(text)
+console.log(completed)
